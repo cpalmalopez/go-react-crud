96d3374842d574aa7597e852ff9917c75f60f41f
Claudio Palma López <xclaudioh@outlook.com>
2022-08-08 17:16:55 -0400

    crud

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 function App() {
+    const [name, setName] = useState("");
+    const [users, setUsers] = useState([]);
+
+    async function loadUsers() {
+        const response = await fetch(import.meta.env.VITE_API + "/users");
+        const data = await response.json();
+        setUsers(data.users)
+    }
+
+    useEffect(() => {
+        loadUsers();
+    }, [])
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await fetch(import.meta.env.VITE_API + "/users", {
+            method: "POST",
+            body: JSON.stringify({ name }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        const data = await response.json()
+        loadUsers();
+    }
+
     return (
         <div>
-            <div>App</div>
-            <button onClick={async () => {
-                const res = await fetch('http://localhost:3000/users')
-                const data = await res.json()
-                console.log(data)
-            }}>Obtener datos</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Nombre' onChange={e => setName(e.target.value)} />
+                <button>Guardar</button>
+            </form>
+
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- main.go
+++ main.go
@@ -1,25 +1,78 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/xclaudioh/go-react-crud/models"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
 	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb"))
+
+	if err != nil {
+		panic(err)
+	}
 
 	app.Use(cors.New())
 
 	app.Static("/", "./client/dist")
 
-	app.Get("/users", func(c *fiber.Ctx) error {
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user models.User
+
+		c.BodyParser(&user)
+
+		coll := client.Database("gomongodb").Collection("users")
+		result, err := coll.InsertOne(context.TODO(), bson.D{{
+			Key:   "name",
+			Value: user.Name,
+		}})
+
+		if err != nil {
+			panic(err)
+		}
+
 		return c.JSON(&fiber.Map{
-			"data": "usuarios desde el back",
+			"data": result,
 		})
 	})
 
-	app.Listen(":3000")
+	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+
+		coll := client.Database("gomongodb").Collection("users")
+		results, err := coll.Find(context.TODO(), bson.M{})
+
+		if err != nil {
+			panic(err)
+		}
+
+		for results.Next(context.TODO()) {
+			var user models.User
+			results.Decode(&user)
+			users = append(users, user)
+		}
+
+		return c.JSON(&fiber.Map{
+			"users": users,
+		})
+	})
+
+	app.Listen(":" + port)
 	fmt.Println("Server on port 3000 a")
 }
